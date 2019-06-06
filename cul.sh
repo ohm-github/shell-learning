c7b42c209a235ec907954cf5e6377fb66cae17da
attapon <dr.piclet@gmail.com>
2019-06-06 16:40:59 +0700

    Initial Commit

---
+++ cul.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+read -p "Input your number 1 : " a
+read -p "Input your number 2 : " b
+
+echo "Total + : $((a+b))"
+echo "Total - : $((a-b))"
+echo "Total * : $((a*b))"
+echo "Total / : $((a/b))"
